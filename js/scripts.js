62173d1876b977115372c64d1fe4d40f4e0bdc85
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-09 13:45:28 -0700

    refactor test two and respective function.

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,13 @@ function Pizza(veggies, meats, cheeses, size) {
   this.size = size;
 }
 
-let Menu = {
-  veggieToppings: [],
-  meatToppings: [],
-  cheeseToppings: []
-}
+function VeggiesAvailable() {}
 
-// function VeggiesAvailable() {
-
-//   veggieToppings.forEach(function(veggie)) {
-//     Pizza.veggies = Pizza.veggies.push()
-//   }
-// }
 
 const veggieToppings = ["Olives", "Red Onions", "Mushrooms", "Bell Pepper", "Fresh Spinach", "Artichokes", "Sun-dried Tomatoes", "Pineapple", "Pesto"]
 const meatToppings = ["Sausage", "Chicken", "Ham", "Bacon", "Pepperoni"]
 const cheeseToppings = ["Parmesan", "Cheddar", "Feta", "Mozzarella", "Smoked Gouda"]
 
-
-// Pizza.prototype.toppingsAvailable = function() {
-//   this.veggies = ["Olives", "Red Onions", "Mushrooms", "Bell Pepper", "Fresh Spinach", "Artichokes", "Sun-dried Tomatoes", "Pineapple", "Pesto"]
-
-// }
 
 
 // // Write out thought process of what needs to happen:
